ec2a7f50877fe79700a6ebb8e79af9536b737a70
tsvetk0va <kknn2821@mail.ru>
2022-08-04 21:09:34 +0300

    initial commit

---
+++ src/test/java/Postman_Echo_Test.java
@@ -0,0 +1,29 @@
+import org.apache.commons.lang3.ObjectUtils;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
+import static org.hamcrest.core.IsEqual.equalTo;
+
+public class Postman_Echo_Test {
+
+
+    @Test
+    void shouldPostSomeUserData() {
+        // Given - When - Then
+        // Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .contentType("text/plain; charset=UTF-8")
+                .body("say hey")
+                // отправляемые данные (заголовки и query можно выставлять аналогично)
+                // Выполняемые действия
+                .when()
+                .post("/post")
+                // Проверки
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("hey"));
+    }
+}
+
